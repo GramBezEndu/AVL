5ea08a90dba33bb4c77d20cd2bcc0b0a3ccf206c
GramBezEndu <33137304+GramBezEndu@users.noreply.github.com>
2018-12-12 20:51:14 +0100

    Add files via upload

--- Drzewo.cs
+++ Drzewo.cs
@@ -44,7 +44,7 @@ namespace AVL
         public Wezel WstawSlowo(ref Wezel root, string slowo)
         {
             //int comparison = slowo.CompareTo(root.Slowo);
-            if (root == null)
+            if(root == null)
             {
                 root = new Wezel(slowo);
             }
@@ -68,120 +68,37 @@ namespace AVL
             {
                 throw new Exception("Slowo znajduje sie juz w zbiorze, synonimy sa niedopuszczalne");
             }
-            if (root.Waga == 2)
+            //sprawdzanie wag
+            if (root.Waga == 0)
+                return root;
+            if (root.Waga >= 2)
             {
                 if (root.Lewy.Waga == 1)
-                {
-                    Wezel wezel = root;
-                    RotacjaRR(ref wezel);
-                    root = (WezelPolski)wezel;
-                    //ustawianie nowych wag
-                    root.Waga = 0;
-                    root.Prawy.Waga = 0;
-                }
+                    RotacjaRR(ref root);
                 else
-                {
-                    Wezel C = root.Lewy.Prawy;
-                    Wezel wezel = root.Lewy;
-                    RotacjaLL(ref wezel);
-                    root.Lewy = (WezelPolski)wezel;
-                    wezel = root;
-                    RotacjaRR(ref wezel);
-                    root = (WezelPolski)wezel;
-                    //ustawianie nowych wag
-                    root.Waga = 0;
-                    switch (C.Waga)
-                    {
-                        case (1):
-
-                            root.Prawy.Waga = 0;
-                            root.Lewy.Waga = -1;
-                            break;
-                        case (0):
-
-                            root.Prawy.Waga = 0;
-                            root.Lewy.Waga = 0;
-                            break;
-                        case (-1):
-                            root.Prawy.Waga = 0;
-                            root.Lewy.Waga = 1;
-                            break;
-
-                    }
-                }
+                    RotacjaLR(ref root);
             }
-            if (root.Waga == -2)
+            if (root.Waga <= -2)
             {
 
                 if (root.Prawy.Waga == 1)
-                {
-                    Wezel C = root.Prawy.Lewy;
-                    Wezel wezel = root.Prawy;
-                    RotacjaRR(ref wezel);
-                    root.Prawy = (WezelPolski)wezel;
-                    wezel = root;
-                    RotacjaLL(ref wezel);
-                    root = (WezelPolski)wezel;
-                    //ustawianie nowych wag
-                    root.Waga = 0;
-                    switch (C.Waga)
-                    {
-                        case (1):
-
-                            root.Prawy.Waga = 0;
-                            root.Lewy.Waga = -1;
-                            break;
-                        case (0):
-
-                            root.Prawy.Waga = 0;
-                            root.Lewy.Waga = 0;
-                            break;
-                        case (-1):
-                            root.Prawy.Waga = 0;
-                            root.Lewy.Waga = 1;
-                            break;
-
-                    }
-
-                }
+                    RotacjaRL(ref root);
                 else
-                {
-                    Wezel wezel = root;
-                    RotacjaLL(ref wezel);
-                    //root = (WezelPolski)wezel;
-                    root = wezel;
-                    //ustawianie nowych wag
-                    root.Waga = 0;
-                    root.Prawy.Waga = 0;
-                }
+                    RotacjaLL(ref root);
             }
             Debug.WriteLine("\t\t{0}", slowo);
             Debug.WriteLine("\t\t{0}", root.Slowo);
             return root;
         }
 
-        public Wezel Wyszukaj(Wezel korzen, string slowo)
-        {
-            // Base Cases: root is null or key is present at root 
-            if (korzen == null || slowo.CompareTo(korzen.Slowo) == 0)
-                return korzen;
-            //return null;
-            // Key is greater than root's key 
-            if (korzen.Slowo.CompareTo(slowo) < 0)
-                return Wyszukaj(korzen.Prawy, slowo);
-
-            //Key is smaller than root's key 
-            return Wyszukaj(korzen.Lewy, slowo);
-        }
-
         public void WypiszDrzewo(Wezel korzen)
         {
+            Console.WriteLine(korzen.Slowo);
             if (korzen.Lewy != null)
             {
                 WypiszDrzewo(korzen.Lewy);
                 //Console.WriteLine(korzen.Lewy.Slowo);
             }
-            Console.WriteLine(korzen.Slowo);
             if (korzen.Prawy != null)
             {
                 WypiszDrzewo(korzen.Prawy);
--- IOManager.cs
+++ IOManager.cs
@@ -22,10 +22,8 @@ namespace AVL
             for(int i=0;i<substrings.Count();i+=2)
             {
                 Debug.WriteLine("\t i={0}, substrings[i]={1}", i, substrings[i]);
-                a.WstawSlowo(ref a.korzen, substrings[i]);
-                var ang = a.Wyszukaj(a.korzen, substrings[i]);
-                p.WstawSlowo(ref p.korzen, substrings[i + 1]);
-                var pl = p.Wyszukaj(p.korzen, substrings[i + 1]);
+                var ang = a.WstawSlowo(ref a.korzen, substrings[i]);
+                var pl = p.WstawSlowo(ref p.korzen, substrings[i + 1]);
                 ang.Tlumaczenie = pl;
                 pl.Tlumaczenie = ang;
                 Debug.WriteLine(pl.Tlumaczenie.Slowo);
@@ -50,6 +48,10 @@ namespace AVL
                 wr = new StreamWriter("InOut0401.txt");
                 wrInitialised = true;
             }
+            if (korzen.Tlumaczenie == null)
+                Console.WriteLine("Brak tlumaczenia dla {0}, nie zostało ono wypisane w pliku wyjsciowym", korzen.Slowo);
+            else
+                wr.WriteLine("{0} {1}", korzen.Slowo, korzen.Tlumaczenie.Slowo);
             if (korzen.Lewy != null)
             {
                 WypiszSlowa(korzen.Lewy);
@@ -58,8 +60,6 @@ namespace AVL
             //if (korzen.Tlumaczenie == null)
             //    throw new Exception("Brak tlumaczenia");
             //else
-            if (korzen.Tlumaczenie != null)
-                wr.WriteLine("{0} {1}",korzen.Slowo, korzen.Tlumaczenie.Slowo);
             if (korzen.Prawy != null)
             {
                 WypiszSlowa(korzen.Prawy);
